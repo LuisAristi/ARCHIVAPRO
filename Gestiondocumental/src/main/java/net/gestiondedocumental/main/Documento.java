fac5960482a18fe1c1f953797613b27f0ea81f2f
S0ske <juanestebanpolobello@gmail.com>
2024-09-18 08:42:01 -0500

    Atributos añadidos en la clase documento

--- Gestiondocumental/src/main/java/net/gestiondedocumental/main/Documento.java
+++ Gestiondocumental/src/main/java/net/gestiondedocumental/main/Documento.java
@@ -9,5 +9,16 @@ package net.gestiondedocumental.main;
  * @author LAB_REDES
  */
 public class Documento {
-    
+   private String nombre; 
+   private String Fechapub; 
+   private String Isbn;
+  private String tipodoc;
+  private String genero;
+  private String autor;
+  private String descrip;
+  private int numpag;
+  private int edicion;
+  private int cantdisp;
+  private double precio;
+  private double valoracion;
 }
