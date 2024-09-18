96b82045a374344045e888514d2897eb5a06b5f4
luis <laristisabala@unicartagena.edu.co>
2024-09-18 08:06:22 -0500

    added  netbeans project

---
+++ Gestiondocumental/src/main/java/net/gestiondedocumental/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.gestiondedocumental.main;
+
+/**
+ *
+ * @author LAB_REDES
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
