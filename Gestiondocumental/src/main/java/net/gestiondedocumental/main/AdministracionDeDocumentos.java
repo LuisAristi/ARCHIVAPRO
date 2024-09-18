97e90b966c61f95969c662c0af03a9243d2f790c
luis <laristisabala@unicartagena.edu.co>
2024-09-18 08:43:08 -0500

    se creo la clase AdministracionDeDocumentos

---
+++ Gestiondocumental/src/main/java/net/gestiondedocumental/main/AdministracionDeDocumentos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.gestiondedocumental.main;
+
+/**
+ *
+ * @author LAB_REDES
+ */
+public class AdministracionDeDocumentos {
+    Documento documento = new Documento();
+    
+}
