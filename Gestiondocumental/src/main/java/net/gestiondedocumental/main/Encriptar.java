3b6abad6fad43cb86bebcb481c10c39626cb714f
luis aristi <santiaristi2015@gmail.com>
2024-10-01 03:00:43 -0500

    basic encription achieved

---
+++ Gestiondocumental/src/main/java/net/gestiondedocumental/main/Encriptar.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.gestiondedocumental.main;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ *
+ * @author lll
+ */
+public class Encriptar {
+    public String password = "hola mundo";
+    public String encrypt(String pw){
+        return BCrypt.hashpw(pw, BCrypt.gensalt());
+    }
+}
